8fdeadea26184e64c999bbd4b1af0ac2ef60f6b1
Mohit Srivastava <iammohitpro@gmail.com>
2020-10-18 14:57:30 +0530

    lol

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+const express = require("express")
+const app = express()
+
+app.listen(process.env.PORT || 3000);
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
