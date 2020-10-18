4083646934e1a6004c9ddf10caf79f16abd6be9c
Mohit Srivastava <iammohitpro@gmail.com>
2020-10-18 11:03:56 +0530

    18

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-const element = <App />;
-
 ReactDOM.render(
-  element,
+  <App />,
   document.getElementById('root')
 );
 
